ca225ea48eab34701a325c63881ced2a1964adb9
Adarsh Anand <adarshchotu09@gmail.com>
2025-04-10 19:58:23 +0530

    Updated Readme

--- pdf_merger.py
+++ pdf_merger.py
@@ -1,6 +1,7 @@
 import PyPDF2
-
+#filess need to be merged and path 
 pdffiles=["Adarsh  Anand 995583.pdf","Adarsh's Resume.pdf","SSPL_CoverLetter.pdf"]
+
 merger=PyPDF2.PdfMerger()
 for filename in pdffiles:
     pdfFile=open(filename,'rb')
